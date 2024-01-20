2c2b3f8f28dd3d3773558ce519aef06564fcb761
zeynel0734 <157225829+zeynel0734@users.noreply.github.com>
2024-01-20 17:58:19 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+      
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ") 
+            
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("bu kelime sözlüğümüzde yok")
